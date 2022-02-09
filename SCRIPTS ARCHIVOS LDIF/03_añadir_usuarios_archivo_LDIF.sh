d070496e533edca69ddb8bb322c527cc1383fbdc
BRUNO LOPEZ BARCIA <BRUNOLB@iesrodolfoucha.es>
2022-02-09 09:38:05 +0100

    Ejercico2 listo

--- SCRIPTS ARCHIVOS LDIF/03_añadir_usuarios_archivo_LDIF.sh
+++ SCRIPTS ARCHIVOS LDIF/03_añadir_usuarios_archivo_LDIF.sh
@@ -11,10 +11,23 @@ read OUNUEVA
 
 USUARIOS=ou=usuarios
 
-echo "dn: ou="$OUNUEVA","$USUARIOS","$DOMINIO >> nuevousuario.ldif
-echo "objectClass: organizationalUnit" >> nuevousuario.ldif
-echo "ou: "$OUNUEVA >> nuevousuario.ldif
-echo "description: prueba de creacion de un archivo ldif con un script" >> nuevousuario.ldif
+echo "dn: cn="$OUNUEVA","$USUARIOS","$DOMINIO >> nuevousuario.ldif
+echo "objectClass: inetOrgPerson" >> nuevousuario.ldif
+echo "objectClass: posixAccount" >> nuevousuario.ldif
+echo "objectClass: shadowAccount" >> nuevousuario.ldif
+echo "sn: apellido" >> nuevousuario.ldif
+echo "cn: "$OUNUEVA >> nuevousuario.ldif
+echo "givenName: "$OUNUEVA >> nuevousuario.ldif
+var_uid=`getent passwd | tail -n 1 | awk -F':' '{print $3}'`
+let "var_uid=var_uid+1"
+echo "uidNumber:"$var_uid >> nuevousuario.ldif
+echo "gidNumber: 10000">> nuevousuario.ldif
+echo "userPassword: abc123." >> nuevousuario.ldif
+echo "gecos: prueba de creacion de un archivo ldif con un script" >> nuevousuario.ldif
+echo "loginShell: /bin/bash" >> nuevousuario.ldif
+echo "homeDirectory: /home/iescalquera/profes/"$OUNUEVA >> nuevousuario.ldif
+echo "INITIALS: P1" >> nuevousuario.ldif
+
 
 ldapadd -D "cn=admin,dc=iescalquera,dc=local" -w abc123. -f nuevousuario.ldif
 
--- SCRIPTS GESTION DE ELEMENTOS/02_borrar_usuarios_grupo.sh
+++ SCRIPTS GESTION DE ELEMENTOS/02_borrar_usuarios_grupo.sh
@@ -1,2 +1,25 @@
 #!/usr/bin/env bash
 
+echo "De que grupo quieres borrar usuarios?"
+read entradagrupo
+
+. ./ayudante2.sh $entradagrupo
+
+cd /root/Scripts/Scripts\ archivos\ LDIF/
+
+. ./01_extraer_nombre_dominio.sh
+
+entradagrupo="g-"$entradagrupo
+
+ldapsearch -x -b cn="$entradagrupo",ou=grupos,"$DOMINIO" | egrep '^member' | awk -F':' '{print $1}' | sed -r 's/\s+//g'  > usuarios_grupo.txt
+
+#Creacion fichero ldif para eliminar los usuarios.
+
+echo "dn: cn=""$entradagrupo"",ou=grupos,""$DOMINIO" >> eleminargrupo.ldif
+echo "changetype: modify" >> eliminargrupo.ldif
+echo "delete: memberUid" >> eliminargrupo.ldif
+
+ldapadd -D "cn=admin,dc=iescalquera,dc=local" -w abc123. -f eliminargrupo.ldif
+
+rm usuarios_grupo.txt 2> /dev/null
+rm eliminargrupo.ldif 2> /dev/null
