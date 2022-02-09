440ea33590e4d43e58b34835221d7a679079c681
BRUNO LOPEZ BARCIA <BRUNOLB@iesrodolfoucha.es>
2022-02-09 10:06:11 +0100

    Añadimos mas modificaciones

---
+++ SCRIPTS CONFIGURACION DIRECTORIOS/01_modifcar_correo.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+#!/usr/bin/env bash
+
+
+cd /root/Scripts/Scripts\ archivos\ LDIF/
+
+. ./01_extraer_nombre_dominio.sh
+
+ getent passwd | awk -F ':' '{if ($3>=10000 && $3<=60000){print $1}}' >> usuariosldap.txt
+
+
+#Creacion fichero ldif para eliminar los usuarios.
+for usuario in $(cat usuariosldap.txt); do
+
+echo "dn: cn=""$usuario"",ou=grupos,""$DOMINIO" >> cambiaremail.ldif
+echo "changetype: modify" >> cambiaremail.ldif
+echo "replace: mail" >> cambiaremail.ldif
+echo "mail: "$usuario"_actualizar@iescalquera.local" >> cambiaremail.ldif
+
+#ldapadd -D "cn=admin,dc=iescalquera,dc=local" -w abc123. -f eliminargrupo.ldif
+
+rm usuarios_grupo.txt 2> /dev/null
+rm eliminargrupo.ldif 2> /dev/null
--- SCRIPTS GESTION DE ELEMENTOS/02_borrar_usuarios_grupo.sh
+++ SCRIPTS GESTION DE ELEMENTOS/02_borrar_usuarios_grupo.sh
@@ -1,25 +1,37 @@
 #!/usr/bin/env bash
 
-echo "De que grupo quieres borrar usuarios?"
-read entradagrupo
-
-. ./ayudante2.sh $entradagrupo
 
 cd /root/Scripts/Scripts\ archivos\ LDIF/
 
 . ./01_extraer_nombre_dominio.sh
 
-entradagrupo="g-"$entradagrupo
+getent passwd | awk -F ':' '{if ($3>=10000 && $3<=60000){print $1}}' > usuariosldap.txt
 
-ldapsearch -x -b cn="$entradagrupo",ou=grupos,"$DOMINIO" | egrep '^member' | awk -F':' '{print $1}' | sed -r 's/\s+//g'  > usuarios_grupo.txt
 
 #Creacion fichero ldif para eliminar los usuarios.
+for usuario in $(cat usuariosldap.txt); do
 
-echo "dn: cn=""$entradagrupo"",ou=grupos,""$DOMINIO" >> eleminargrupo.ldif
-echo "changetype: modify" >> eliminargrupo.ldif
-echo "delete: memberUid" >> eliminargrupo.ldif
+echo "dn: uid="$usuario",ou=usuarios,""$DOMINIO" >> cambiaremail.ldif
+echo "changetype: modify" >> cambiaremail.ldif
+echo "replace: mail" >> cambiaremail.ldif
+echo "mail: "$usuario"_actualizar@iescalquera.local" >> cambiaremail.ldif
 
-ldapadd -D "cn=admin,dc=iescalquera,dc=local" -w abc123. -f eliminargrupo.ldif
+ldapadd -D "cn=admin,dc=iescalquera,dc=local" -w abc123. -f cambiaremail.ldif 2> /dev/null
 
-rm usuarios_grupo.txt 2> /dev/null
-rm eliminargrupo.ldif 2> /dev/null
+rm cambiaremail.ldif 2> /dev/null
+done
+
+#Creacion fichero ldif para eliminar los usuarios.
+for usuario in $(cat usuariosldap.txt); do
+
+echo "dn: uid="$usuario",ou=profes"",ou=usuarios,""$DOMINIO" >> cambiaremail.ldif
+echo "changetype: modify" >> cambiaremail.ldif
+echo "replace: mail" >> cambiaremail.ldif
+echo "mail: "$usuario"_actualizar@iescalquera.local" >> cambiaremail.ldif
+
+ldapadd -D "cn=admin,dc=iescalquera,dc=local" -w abc123. -f cambiaremail.ldif 2> /dev/null
+
+rm cambiaremail.ldif 2> /dev/null
+done
+
+rm usuariosldap.txt 2> /dev/null
